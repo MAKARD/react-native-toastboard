d2e2fc2987af7cd446397850d7f90730a78774ea
MAKARD <human.getEmail@gmail.com>
2018-12-16 18:39:31 +0200

    Write tests for 'wait'

---
+++ __tests__/utils.wait.spec.js
@@ -0,0 +1,40 @@
+/* eslint-disable no-undef */
+import React from "react";
+
+import { wait } from "../src/utils/wait";
+
+describe("utils/wait", () => {
+	test("Should resolve promise after 2000 sec delay", () => {
+		jest.useFakeTimers();
+
+		wait();
+		expect(setTimeout).toHaveBeenCalledTimes(1);
+		expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 2000);
+
+		jest.clearAllTimers();
+	});
+
+	test("Should resolve promise after specified delay", () => {
+		jest.useFakeTimers();
+
+		wait(100);
+		expect(setTimeout).toHaveBeenCalledTimes(1);
+		expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 100);
+
+		jest.clearAllTimers();
+	});
+
+	test("Should manual resolve promise on callback", () => {
+		jest.useFakeTimers();
+
+		wait(100, (resolve) => {
+			resolve();
+		});
+
+		expect(setTimeout).toHaveBeenCalledTimes(1);
+		expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 100);
+		expect(clearTimeout).toHaveBeenCalledTimes(1);
+
+		jest.clearAllTimers();
+	});
+});
