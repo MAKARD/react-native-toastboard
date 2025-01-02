9d4f6a8f81a0e7d801274a42563a9039e0394ad1
MAKARD01 <dmytro.makarenko@kingfisher.com>
2025-01-02 17:15:57 +0200

    Update deps

--- src/components/Toaster/__tests__/useToaster.test.ts
+++ src/components/Toaster/__tests__/useToaster.test.ts
@@ -1,4 +1,4 @@
-import { renderHook, act } from '@testing-library/react-hooks';
+import { renderHook, act } from '@testing-library/react-native';
 
 import { StaticMethods, useToaster } from '../useToaster';
 import { SlideX } from '../../../animations';
